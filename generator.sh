3fd8a75ed3ef096d42a5bc50068ddb56815898ab
moonagic <i@moonagic.com>
2021-12-27 12:48:37 +0800

    Update

--- generator.sh
+++ generator.sh
@@ -2,4 +2,5 @@
 package-collection-generate packages.json temp_collection.json
 jq . temp_collection.json > collection.json
 rm temp_collection.json
-
+./package-collection-sign collection.json temp_signed-collection.json key.pem swift_package.cer
+jq . temp_signed-collection.json > signed-collection.json
